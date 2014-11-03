6054cc8560ae5e57e86124b5582a958666f08af0
Tadas <tadcka89@gmail.com>
2014-11-03 22:45:26 +0200

    CS.

--- DependencyInjection/TadckaRoutingExtension.php
+++ DependencyInjection/TadckaRoutingExtension.php
@@ -13,7 +13,6 @@ namespace Tadcka\Bundle\RoutingBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
